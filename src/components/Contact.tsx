edb1e88a31d7046807317e48299ffa4a5f519001
ahm9dzakaria <ahmed.zakaria@united-tech.org>
2025-07-26 21:22:42 +0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -34,6 +34,10 @@ const Contact = () => {
                     <p className="text-muted-foreground font-cairo">+20 1222919936</p>
                   </div>
                 </div>
+                 <div className="space-y-6">
+                <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                  <div className="w-12 h-12 bg-gradient-to-br from-primary to-primary-glow rounded-lg flex items-center justify-center">
+                    <Phone className="w-6 h-6 text-white" />
                 <div>
                     <p className="font-semibold font-cairo">الهاتف</p>
                     <p className="text-muted-foreground font-cairo">+20 1140764800</p>
